4894d761ad33b014fb86c2ce41ad6ba7d9a14150
luoqiang <luoqiang@sakuragame.net>
2024-08-15 10:45:24 +0800

    feat: 新增和优化日志输出格式

--- logs.go
+++ logs.go
@@ -2,6 +2,7 @@ package zapPitaya
 
 import (
 	"context"
+	"fmt"
 	"github.com/topfreegames/pitaya/v2"
 	"github.com/topfreegames/pitaya/v2/constants"
 	"github.com/topfreegames/pitaya/v2/logger/interfaces"
@@ -10,12 +11,18 @@ import (
 
 // CTX 定义用户区分数据的管理
 // 如果key是zapCtx则在WithField和WithFields时取出pitaya的上下文数据并提取
-const CTX = "zapPitayaCtx"
+const CTX = "zapCtx"
+const SESSION_UID = "session_uid"
 
-// InitZapLogger 初始化zap
+// GetPitayaLogger 获得pitaya的logger
 // 这里只实现pitaya接口
-func SetZapLog(logger *zap.Logger) interfaces.Logger {
-	return &ZapLog{logger: logger}
+func GetPitayaLogger(zap *zap.Logger) interfaces.Logger {
+	return &ZapLog{logger: zap}
+}
+
+// NewDefaultPitayaLogger 使用zap example 实现
+func NewDefaultPitayaLogger() interfaces.Logger {
+	return &ZapLog{logger: zap.NewExample()}
 }
 
 // ZapLog zap log
@@ -27,7 +34,7 @@ func (z *ZapLog) Fatal(format ...interface{}) {
 	z.logger.Sugar().Fatal(format)
 }
 func (z *ZapLog) Fatalf(format string, args ...interface{}) {
-	z.logger.Sugar().Fatal(format)
+	z.logger.Sugar().Fatal(fmt.Sprintf(format, args...))
 }
 func (z *ZapLog) Fatalln(args ...interface{}) {
 	z.logger.Sugar().Fatalln(args)
@@ -37,7 +44,7 @@ func (z *ZapLog) Debug(args ...interface{}) {
 	z.logger.Sugar().Debug(args)
 }
 func (z *ZapLog) Debugf(format string, args ...interface{}) {
-	z.logger.Sugar().Debugf(format, args)
+	z.logger.Sugar().Debugf(fmt.Sprintf(format, args...))
 }
 func (z *ZapLog) Debugln(args ...interface{}) {
 	z.logger.Sugar().Debugln(args)
@@ -47,7 +54,7 @@ func (z *ZapLog) Error(args ...interface{}) {
 	z.logger.Sugar().Error(args)
 }
 func (z *ZapLog) Errorf(format string, args ...interface{}) {
-	z.logger.Sugar().Errorf(format, args)
+	z.logger.Sugar().Errorf(fmt.Sprintf(format, args...))
 }
 func (z *ZapLog) Errorln(args ...interface{}) {
 	z.logger.Sugar().Errorln(args)
@@ -57,7 +64,7 @@ func (z *ZapLog) Info(args ...interface{}) {
 	z.logger.Sugar().Info(args)
 }
 func (z *ZapLog) Infof(format string, args ...interface{}) {
-	z.logger.Sugar().Infof(format, args)
+	z.logger.Sugar().Infof(fmt.Sprintf(format, args...))
 }
 func (z *ZapLog) Infoln(args ...interface{}) {
 	z.logger.Sugar().Infoln(args)
@@ -67,7 +74,7 @@ func (z *ZapLog) Warn(args ...interface{}) {
 	z.logger.Sugar().Warn(args)
 }
 func (z *ZapLog) Warnf(format string, args ...interface{}) {
-	z.logger.Sugar().Warnf(format, args)
+	z.logger.Sugar().Warnf(fmt.Sprintf(format, args...))
 }
 func (z *ZapLog) Warnln(args ...interface{}) {
 	z.logger.Sugar().Warnln(args)
@@ -77,44 +84,96 @@ func (z *ZapLog) Panic(args ...interface{}) {
 	z.logger.Sugar().Panic(args)
 }
 func (z *ZapLog) Panicf(format string, args ...interface{}) {
-	z.logger.Sugar().Panicf(format, args)
+	z.logger.Sugar().Panicf(fmt.Sprintf(format, args...))
 }
 func (z *ZapLog) Panicln(args ...interface{}) {
 	z.logger.Sugar().Panicln(args)
 }
 
+// WithFields 日志添加列
+// 如果是自定义上下文key则特殊处理
 func (z *ZapLog) WithFields(fields map[string]interface{}) interfaces.Logger {
+	fieldsList := make([]zap.Field, 0, len(fields))
+	for k, v := range fields {
+		if k == CTX {
+			ctx, ok := v.(context.Context)
+			if !ok {
+				continue
+			}
 
-	return z
+			// ready
+			peerIDKey := pitaya.GetFromPropagateCtx(ctx, constants.PeerIDKey)
+			routeKey := pitaya.GetFromPropagateCtx(ctx, constants.RouteKey)
+			peerServiceKey := pitaya.GetFromPropagateCtx(ctx, constants.PeerServiceKey)
+			requestIDKey := pitaya.GetFromPropagateCtx(ctx, constants.RequestIDKey)
+			startTimeKey := pitaya.GetFromPropagateCtx(ctx, constants.StartTimeKey)
+
+			// append
+			fieldsList = append(fieldsList, zap.Any(constants.PeerIDKey, peerIDKey))
+			fieldsList = append(fieldsList, zap.Any(constants.RouteKey, routeKey))
+			fieldsList = append(fieldsList, zap.Any(constants.PeerServiceKey, peerServiceKey))
+			fieldsList = append(fieldsList, zap.Any(constants.RequestIDKey, requestIDKey))
+			fieldsList = append(fieldsList, zap.Any(constants.StartTimeKey, startTimeKey))
+
+			// uid
+			session := pitaya.GetSessionFromCtx(ctx)
+			if session != nil {
+				fieldsList = append(fieldsList, zap.Any(SESSION_UID, session.UID()))
+			} else {
+				fieldsList = append(fieldsList, zap.Any(SESSION_UID, nil))
+			}
+		} else {
+			fieldsList = append(fieldsList, zap.Any(k, v))
+		}
+	}
+
+	logger := z.logger.With(fieldsList...)
+	return &ZapLog{logger: logger}
 }
+
+// WithField 日志添加列
+// 如果是自定义上下文key则特殊处理
 func (z *ZapLog) WithField(key string, value interface{}) interfaces.Logger {
 
+	newZap := &ZapLog{}
 	if key == CTX {
 		ctx, ok := value.(context.Context)
 		if !ok {
 			return z
 		}
 
-		uid := pitaya.GetSessionFromCtx(ctx).UID()
-		route := pitaya.GetFromPropagateCtx(ctx, constants.RouteKey)
-		service := pitaya.GetFromPropagateCtx(ctx, constants.PeerServiceKey)
-		requestID := pitaya.GetFromPropagateCtx(ctx, constants.RequestIDKey)
-		logger := z.logger.With(zap.Any("route", route),
-			zap.Any("service", service),
-			zap.Any("requestID", requestID),
-			zap.Any("uid", uid))
-		return &ZapLog{
-			logger: logger,
+		// 上下文内数据
+		peerIDKey := pitaya.GetFromPropagateCtx(ctx, constants.PeerIDKey)
+		routeKey := pitaya.GetFromPropagateCtx(ctx, constants.RouteKey)
+		peerServiceKey := pitaya.GetFromPropagateCtx(ctx, constants.PeerServiceKey)
+		requestIDKey := pitaya.GetFromPropagateCtx(ctx, constants.RequestIDKey)
+		startTimeKey := pitaya.GetFromPropagateCtx(ctx, constants.StartTimeKey)
+		logger := z.logger.With(
+			zap.Any(constants.PeerIDKey, peerIDKey),
+			zap.Any(constants.RouteKey, routeKey),
+			zap.Any(constants.PeerServiceKey, peerServiceKey),
+			zap.Any(constants.RequestIDKey, requestIDKey),
+			zap.Any(constants.StartTimeKey, startTimeKey),
+		)
+
+		// uid
+		session := pitaya.GetSessionFromCtx(ctx)
+		if session != nil {
+			logger = logger.With(zap.Any(SESSION_UID, session.UID()))
+		} else {
+			logger = logger.With(zap.Any(SESSION_UID, nil))
 		}
+
+		newZap.logger = logger
+	} else {
+		newZap.logger = z.logger.With(zap.Any(key, value))
 	}
 
-	logger := z.logger.With(zap.Any(key, value))
-	return &ZapLog{
-		logger: logger,
-	}
+	return newZap
 }
+
 func (z *ZapLog) WithError(err error) interfaces.Logger {
-	return z
+	return &ZapLog{logger: z.logger.With(zap.Error(err))}
 }
 
 func (z *ZapLog) GetInternalLogger() any {
---
+++ logs_test.go
@@ -0,0 +1,65 @@
+package zapPitaya
+
+import (
+	"context"
+	"fmt"
+	"go.uber.org/zap"
+	"testing"
+)
+
+func TestGetPitayaLogger(t *testing.T) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	m := make(map[string]interface{})
+	m["test"] = "test"
+	m["num"] = 100
+
+	log := GetPitayaLogger(logger)
+
+	log.Debug("this is Debug", "21212")
+	log.Debugf("this is Debugf, %s, %d, %+v", "string", 1000, m)
+	log.Debugln("this is Debugln")
+
+	log.Error(fmt.Sprintf("122222"))
+	log.Errorf(" %s, %d, %v", "string", 1000, m)
+	log.Errorln("this is Errorln")
+
+	log.Info("this is Info")
+	log.Infof("this is Infof, %s, %d, %v", "string", 1000, m)
+	log.Infoln("this is Infoln")
+
+	log.Warn("this is Warn")
+	log.Warnf("this is Warnf, %s, %d, %v", "string", 1000, m)
+	log.Warnln("this is Warnln")
+
+	log.Panic("this is Panic")
+	log.Panicf("this is Panicf, %s, %d, %v", "string", 1000, m)
+	log.Panicln("this is Panicln")
+
+	log.Fatal("this is Fatal")
+	log.Fatalf("this is Fatalf, %s, %d, %v", "string", 1000, m)
+	log.Fatalln("this is Fatalln")
+}
+
+func TestWithFile(t *testing.T) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	m := make(map[string]interface{})
+	m["test"] = "test"
+	m["num"] = 100
+
+	log := GetPitayaLogger(logger)
+	log = log.WithField("test", "test")
+	log = log.WithError(fmt.Errorf("test err"))
+	log = log.WithField(CTX, context.TODO())
+	log = log.WithField("err", fmt.Errorf("test err2"))
+	log.Error("this is Info")
+	log.Errorf("this is Infof, %s, %d, %+v", "string", 1000, m)
+	log.Errorln("this is Infoln ")
+}
