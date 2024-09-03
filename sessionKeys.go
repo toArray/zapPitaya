4c57fc23d80d2b30a37d678402981e0a13659da9
luoqiang <luoqiang@sakuragame.net>
2024-09-03 11:40:24 +0800

    feat: 新增sessionkey

---
+++ sessionKeys.go
@@ -0,0 +1,15 @@
+package zapPitaya
+
+// 自定义到session的数据
+const (
+	Uuid           = "uuid"           // 用户UUID
+	UserIP         = "userIP"         // 用户IP
+	UserID         = "userID"         // 用户ID
+	RegionID       = "regionID"       // 区服ID
+	PackageID      = "packageID"      // 包体ID
+	ResVersion     = "resVersion"     // 热更版本
+	PackageVersion = "packageVersion" // 包体版本
+	GameNodeID     = "gameNodeID"     // 游戏节点ID
+	Channel        = "channel"        // 渠道
+	APP            = "app"            // 应用
+)
